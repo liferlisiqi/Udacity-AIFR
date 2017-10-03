c18e2fbbddc0e6ec02db37a635f6d9f7bed1be0d
liferlisiqi <lisiqi9310@163.com>
2017-10-03 22:21:30 +0800

    find the way, ok

--- search.py
+++ search.py
@@ -15,27 +15,46 @@
 #   0 = Navigable space
 #   1 = Occupied space
 
+# grid = [[0, 0, 1, 0, 0, 0],
+#         [0, 0, 1, 0, 0, 0],
+#         [0, 0, 0, 0, 1, 0],
+#         [0, 0, 1, 1, 1, 0],
+#         [0, 0, 0, 0, 1, 0]]
+
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
+
+# grid = [[0, 1, 1, 1, 1],
+#         [0, 1, 0, 0, 0],
+#         [0, 0, 0, 1, 0],
+#         [1, 1, 1, 1, 0],
+#         [0, 0, 0, 1, 0]]
+# unconnected component, how to do? don't warry, is not have to consider
 init = [0, 0]
-goal = [len(grid)-1, len(grid[0])-1]  # [4, 5]
+goal = [len(grid) - 1, len(grid[0]) - 1]  # [4, 5]
 cost = 1
 
-delta = [[-1, 0], # go up
-         [ 0,-1], # go left
-         [ 1, 0], # go down
-         [ 0, 1]] # go right
+delta = [[-1, 0],  # go up
+         [0, -1],  # go left
+         [1, 0],  # go down
+         [0, 1]]  # go right
 
 delta_name = ['^', '<', 'v', '>']
 
-def search(grid,init,goal,cost):
+
+def search(grid, init, goal, cost):
     row = len(grid)
     col = len(grid[0])
     open_list = [[0, init[0], init[1]]]
-    # so ugly, change to one list with element of list of three element
+    path = [[' ' for c in range(col)] for r in range(row)]
+    # expand = [[-1 for c in range(col)] for r in range(row)]
+    # expand_number = 0
+    # print expand
+    # what is the order of init of 2D array?is he wrong? mistake col with row? yes, i am right
+
     while len(open_list) != 0:
         g_min_index = 0
         for i in range(1, len(open_list)):
@@ -46,6 +65,8 @@ def search(grid,init,goal,cost):
         x = next[1]
         y = next[2]
         grid[x][y] = 1
+        # expand_number += 1
+        # expand[x][y] = expand_number
 
         for i in range(len(delta)):
             x2 = x + delta[i][0]
@@ -55,13 +76,32 @@ def search(grid,init,goal,cost):
                 # if the grid is occupied and is not the goal
                 if grid[x2][y2] == 0 and (x2 != goal[0] or y2 != goal[1]):
                     grid[x2][y2] = 1
+                    path[x2][y2] = delta_name[i]
                     open_list.append([g + cost, x2, y2])
                 # find the goal
                 elif x2 == goal[0] and y2 == goal[1]:
-                    return str([g + cost, x2, y2])
-        print "expand gird: " + str(next)
-        print "open list: " + str(open_list)
+                    path[x2][y2] = delta_name[i]
+                    return path
+                    #return str([g + cost, x2, y2])
+        #print "expand gird: " + str(next)
+        #print "open list: " + str(open_list)
+                    # return str(expand)
 
-    return 'fail'
-# why my output is wrong?
-print search(grid, init, goal, cost)
+    return path
+
+# return the path, must be the same ? it is ridiculous
+def path(deltas):
+    paths = [[' ' for c in range(len(deltas[0]))] for r in range(len(deltas))]
+    paths[goal[0]][goal[1]] = '*'
+    grid = [goal[0], goal[1]]
+    while grid != init:
+        delta_temp = deltas[grid[0]][grid[1]]
+        delta_index = delta_name.index(delta_temp)
+        grid = [grid[0] - delta[delta_index][0], grid[1] - delta[delta_index][1]]
+        paths[grid[0]][grid[1]] = delta_temp
+    return paths
+
+
+# why my output is wrong? no
+deltas = search(grid, init, goal, cost)
+print path(deltas)
