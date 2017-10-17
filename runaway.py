7ab4fd4a2c79750cac7a6a7be4ee8f001348d789
liferlisiqi <lisiqi9310@163.com>
2017-10-17 16:56:28 +0800

    fuck me

--- runaway.py
+++ runaway.py
@@ -15,8 +15,8 @@
 # This series of questions involves the recovery of a rogue Traxbot. This bot has
 # gotten lost somewhere in the desert and is now stuck driving in an almost-circle: it has
 # been repeatedly driving forward by some step size, stopping, turning a certain
-# amount, and repeating this process... Luckily, the Traxbot is still sending all
-# of its sensor data back to headquarters.
+# amount, and repeating this process...
+# Luckily, the Traxbot is still sending all of its sensor data back to headquarters.
 #
 # In this project, we will start with a simple version of this problem and
 # gradually add complexity. By the end, you will have a fully articulated
@@ -35,6 +35,19 @@
 # 1 Figure out the heading angle, step size and the turning angle that Traxbot is moving with.
 # 2 Complete the estimate_next_pos function.
 #
+# ----------
+# Part Two
+#
+# Now we'll make the scenario a bit more realistic. Now Traxbot's
+# sensor measurements are a bit noisy (though its motions are still
+# completetly noise-free and it still moves in an almost-circle).
+# You'll have to write a function that takes as input the next
+# noisy (x, y) sensor measurement and outputs the best guess
+# for the robot's next position.
+#
+# ----------
+# YOUR JOB
+# Complete the function estimate_next_pos with noisy measurement
 
 from robot import *
 from math import *
@@ -42,20 +55,13 @@ from matrix import *
 import random
 import turtle
 
-
 # This function will have to be called multiple times before you
 # have enough information to accurately predict the next position.
-# The OTHER variable that your function returns will be
-# passed back to your function the next time it is called.
-# You can use this to keep track of important information over time.
-def estimate_next_pos(measurement, heading, turning, distance):
-    """Estimate the next (x, y) position of the wandering Traxbot
-    based on noisy (x, y) measurements."""
-    estimate_bot = robot(measurement[0], measurement[1], heading, turning, distance)
-    estimate_bot.move_in_circle()
-
-    return (estimate_bot.x, estimate_bot.y)
-
+def estimate_next_pos(measurement, OTHER):
+    if not OTHER: # this is the first measurement
+        OTHER = measurement
+    xy_estimate = OTHER
+    return xy_estimate, OTHER
 
 # A helper function you may find useful.
 def distance_between(point1, point2):
@@ -64,12 +70,11 @@ def distance_between(point1, point2):
     x2, y2 = point2
     return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-
 # Note that the OTHER variable allows you to store any information that you want.
 # Visualization: the red circle represents the measured position of the broken robot,
 # the blue arrow represents your prediction for where the broken robot will be,
 # and the green turtle represents the true position of where the broken robot will be.
-def demo_grading(estimate_next_pos_fcn, target_bot, OTHER=None):
+def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
     localized = False
     distance_tolerance = 0.01 * target_bot.distance
     ctr = 0
@@ -105,8 +110,7 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER=None):
     while not localized and ctr <= 10:
         ctr += 1
         measurement = target_bot.sense()
-
-        position_guess = estimate_next_pos_fcn(measurement, target_bot.heading, target_bot.turning, target_bot.distance)
+        position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
         target_bot.move_in_circle()
         true_position = (target_bot.x, target_bot.y)
         error = distance_between(position_guess, true_position)
@@ -130,10 +134,11 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER=None):
         # End of Visualization
     return localized
 
-
 # This is how we create a target bot. Check the robot.py file to understand
 # How the robot class behaves.
 test_target = robot(2.1, 4.3, 0.5, 2 * pi / 34.0, 1.5)  # x, y, heading, turning, distance
 test_target.set_noise(0.0, 0.0, 0.0)  # no noise
+# measurement_noise = 0.05 * test_target.distance
+# test_target.set_noise(0.0, 0.0, measurement_noise)
 
 demo_grading(estimate_next_pos, test_target)
